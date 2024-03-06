64472fb0e2bb94712d40efce3269025dad9bc3f6
Valeria Shevtsova <vsshevtsova@yandex.ru>
2024-03-06 23:47:25 +0400

    first initialization

---
+++ js/script.js
@@ -0,0 +1,115 @@
+// Автоматизировать вопросы пользователю про фильмы при помощи цикла
+// Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов.
+// Если это происходит - возвращаем пользователя к вопросам опять.
+
+// При помощи условий проверить personalMovieDB.count,
+// и если он меньше 10 - вывести сообщение 'Просмотрено довольно мало фильмов',
+// если от 10 до 30 - 'Вы классический зритель',
+// а если больше - 'Вы киноман'.
+// А если не подошло ни к одному варианту - 'Произошла ошибка'
+// Потренироваться и переписать цикл еще двумя способами
+
+let numberOfFilms;
+let film = "";
+let rate;
+
+//Первый вариант
+
+do {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+} while (numberOfFilms === "" || numberOfFilms === null);
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: [],
+  genres: false,
+};
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
+
+do {
+  film = prompt("Один из последних просмотренных фильмов?");
+  rate = +prompt("На сколько оцените его?");
+} while (
+  film === "" ||
+  film === null ||
+  film.length > 50 ||
+  rate === "" ||
+  rate === 0
+);
+personalMovieDB.movies[`"${film}"`] = `${rate}`;
+console.log(personalMovieDB);
+
+// Второй вариант
+
+// for (; numberOfFilms == null || numberOfFilms == ""; ) {
+//   numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// }
+
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: [],
+//   genres: false,
+// };
+
+// if (personalMovieDB.count < 10) {
+//   alert("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//   alert("Вы классический зритель");
+// } else if (personalMovieDB.count > 30) {
+//   alert("Вы киноман");
+// } else {
+//   alert("Произошла ошибка");
+// }
+
+// for (
+//   ;
+//   film === "" || film === null || film.length > 50 || rate === "" || rate === 0;
+
+// ) {
+//   film = prompt("Один из последних просмотренных фильмов?");
+//   rate = +prompt("На сколько оцените его?");
+// }
+// personalMovieDB.movies[`"${film}"`] = `${rate}`;
+// console.log(personalMovieDB);
+
+//Третий вариант
+
+// while (numberOfFilms == null || numberOfFilms == "") {
+//   numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// }
+
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: [],
+//   genres: false,
+// };
+
+// if (personalMovieDB.count < 10) {
+//   alert("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//   alert("Вы классический зритель");
+// } else if (personalMovieDB.count > 30) {
+//   alert("Вы киноман");
+// } else {
+//   alert("Произошла ошибка");
+// }
+
+// while (film === "" || film === null || film.length > 50 || rate === "" || rate === 0) {
+//   film = prompt("Один из последних просмотренных фильмов?");
+//   rate = +prompt("На сколько оцените его?");
+// }
+// personalMovieDB.movies[`"${film}"`] = `${rate}`;
+// console.log(personalMovieDB);
